6af99f331c4d4256558434c0962b70e14948dec5
Rachel Veer <rachelyoubish@gmail.com>
2021-05-21 12:30:11 -0500

    No pointers needed.

--- Engine/src/Platform/Windows/PlatformWin32.cpp
+++ Engine/src/Platform/Windows/PlatformWin32.cpp
@@ -19,8 +19,8 @@ typedef struct Win32Props // Win32 Properties.
     const std::wstring wndClass = L"Engine Window Class";
 } Win32Props;
 
-Win32Props* win32props = new Win32Props;
-Clock* winclock = new Clock;
+Win32Props win32props;
+Clock winclock;
 
 LRESULT CALLBACK Win32ProcessMessages(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -31,13 +31,14 @@ void Platform::Startup(
     int32_t width,
     int32_t height)
 {
+    
     // Windows 10 Creators update adds Per Monitor V2 DPI awareness context.
     // Using this awareness context allows the client area of the window 
     // to achieve 100% scaling while still allowing non-client window content to 
     // be rendered in a DPI sensitive fashion.
     SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 
-    win32props->hInstance = GetModuleHandle(0);
+    win32props.hInstance = GetModuleHandle(0);
     
     // Register the window class.
     WNDCLASSEX wc = {};
@@ -47,12 +48,12 @@ void Platform::Startup(
     wc.lpfnWndProc = Win32ProcessMessages;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
-    wc.hInstance = win32props->hInstance;
+    wc.hInstance = win32props.hInstance;
     wc.hIcon = NULL;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = NULL;
     wc.lpszMenuName = NULL;
-    wc.lpszClassName = win32props->wndClass.c_str();
+    wc.lpszClassName = win32props.wndClass.c_str();
     wc.hIconSm = NULL;
 
     if (!RegisterClassEx(&wc))
@@ -61,9 +62,9 @@ void Platform::Startup(
     };
 
     // Create the window.
-    win32props->hWnd = CreateWindowExW(
+    win32props.hWnd = CreateWindowExW(
         0,                              // Optional window styles
-        win32props->wndClass.c_str(),   // Window class
+        win32props.wndClass.c_str(),   // Window class
         applicationName,                // Window text
         (WS_OVERLAPPED | WS_CAPTION
         | WS_SYSMENU | WS_MINIMIZEBOX), // Window style
@@ -71,13 +72,13 @@ void Platform::Startup(
         width, height,                  // Size
         nullptr,                        // Parent window
         nullptr,                        // Menu
-        win32props->hInstance,          // Instance handle
+        win32props.hInstance,          // Instance handle
         0                               // Additional application data
     );
 
     // Quick and dirty error checking.
     // If failed, indicate via message box, otherwise show now created window. 
-    if (win32props->hWnd == NULL)
+    if (win32props.hWnd == NULL)
     {
         MessageBox(NULL, L"Window creation failed", L"Error", 0);
     }
@@ -85,22 +86,22 @@ void Platform::Startup(
     {
         // Window is hidden by default, thus we have to specify showing it.
         // Could be SW_SHOW instead of nCmdShow.
-        ShowWindow(win32props->hWnd, SW_SHOW);
+        ShowWindow(win32props.hWnd, SW_SHOW);
     }
 
     // Clock setup.
     LARGE_INTEGER frequency;
     QueryPerformanceFrequency(&frequency);
-    winclock->ClockFrequency = 1.0f / (double)frequency.QuadPart;
-    QueryPerformanceCounter(&winclock->StartTime);
+    winclock.ClockFrequency = 1.0f / (double)frequency.QuadPart;
+    QueryPerformanceCounter(&winclock.StartTime);
 }
 
 void Platform::Shutdown()
 {
-    if (win32props->hWnd)
+    if (win32props.hWnd)
     {
-        DestroyWindow(win32props->hWnd);
-        win32props->hWnd = 0;
+        DestroyWindow(win32props.hWnd);
+        win32props.hWnd = 0;
     }
 }
 
@@ -128,14 +129,14 @@ double Platform::GetAbsoluteTime() const
 {
     LARGE_INTEGER currentTime;
     QueryPerformanceCounter(&currentTime);
-    return (double)currentTime.QuadPart * winclock->ClockFrequency;
+    return (double)currentTime.QuadPart * winclock.ClockFrequency;
 }
 
 double Platform::Peek() const
 {
     LARGE_INTEGER currentTime;
     QueryPerformanceCounter(&currentTime);
-    double elapsedTime = (currentTime.QuadPart - winclock->StartTime.QuadPart) * winclock->ClockFrequency;
+    double elapsedTime = (currentTime.QuadPart - winclock.StartTime.QuadPart) * winclock.ClockFrequency;
     return elapsedTime;
 }
 
