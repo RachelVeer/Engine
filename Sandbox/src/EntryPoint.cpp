4661f0cda9343cc0d0704374c895e33e9d4504b6
Rachel Veer <rachelyoubish@gmail.com>
2021-05-23 16:32:46 -0500

    Rename to Seacrest. :^)

--- Sandbox/src/EntryPoint.cpp
+++ Sandbox/src/EntryPoint.cpp
@@ -9,7 +9,7 @@ void CreateGame(Game* OutGame)
     OutGame->appConfig.startPosY = 100;
     OutGame->appConfig.startWidth = 1280;
     OutGame->appConfig.startHeight = 720;
-    OutGame->appConfig.Name = L"Sandbox";
+    OutGame->appConfig.Name = L"Seacrest";
     OutGame->Update = SandboxUpdate;
     OutGame->Render = SandboxRender;
     OutGame->Initialize = SandboxInitialize;
