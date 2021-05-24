ede8b7596499811135c8e9687e4582197e6b6553
Rachel Veer <rachelyoubish@gmail.com>
2021-05-24 11:02:46 -0500

    Sandbox entrypoint & configuration: Explicit values.

--- Sandbox/src/EntryPoint.cpp
+++ Sandbox/src/EntryPoint.cpp
@@ -1,21 +1,35 @@
 #include "Sandbox.h"
 #include <EntryPoint.h>
 
+typedef struct SandboxConfiguration
+{
+    int16_t startPosX = 100;
+    int16_t startPosY = 100;
+    int16_t startWidth = 1280;
+    int16_t startHeight = 720;
+    const wchar_t* Name = L"Seacrest Engine Sandbox";
+} SandboxConfiguration;
+
 // Define the function to create a game.
 void CreateGame(Game* OutGame)
 {
+    // Sandbox has its own "config", to 
+    // prevent passing in "magic" numbers. 
+    SandboxConfiguration sandboxConfig;
+
     // Application configuration.
-    OutGame->appConfig.startPosX = 100;
-    OutGame->appConfig.startPosY = 100;
-    OutGame->appConfig.startWidth = 1280;
-    OutGame->appConfig.startHeight = 720;
-    OutGame->appConfig.Name = L"Seacrest Engine Sandbox";
-    OutGame->gfx = GFXAPI::Direct3D12;
-    OutGame->gfxContext = {};
+    OutGame->appConfig.startPosX   = sandboxConfig.startPosX;
+    OutGame->appConfig.startPosY   = sandboxConfig.startPosY;
+    OutGame->appConfig.startWidth  = sandboxConfig.startWidth;
+    OutGame->appConfig.startHeight = sandboxConfig.startHeight;
+    OutGame->appConfig.Name        = sandboxConfig.Name;
+    // Renderer.
+    OutGame->gfx                   = GFXAPI::Direct3D12;
+    OutGame->gfxContext            = {};
     // Function pointers. 
-    OutGame->Update = SandboxUpdate;
-    OutGame->Render = SandboxRender;
-    OutGame->Initialize = SandboxInitialize;
+    OutGame->Update                = SandboxUpdate;
+    OutGame->Render                = SandboxRender;
+    OutGame->Initialize            = SandboxInitialize;
 
     // Create the game state.
     OutGame->state = malloc(sizeof(SandboxState));
