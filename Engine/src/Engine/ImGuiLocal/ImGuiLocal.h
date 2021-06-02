8044b802e29016a0184ce2bd0e3ffda6467177ce
Rachel Veer <rachelyoubish@gmail.com>
2021-06-02 06:22:17 -0500

    Imgui: Imgui interface handles demo code + control over both begin & end frame now.

--- Engine/src/Engine/ImGuiLocal/ImGuiLocal.h
+++ Engine/src/Engine/ImGuiLocal/ImGuiLocal.h
@@ -4,6 +4,8 @@
 #include <imgui/backends/imgui_impl_win32.h>
 #include <imgui/backends/imgui_impl_dx12.h>
 
+struct ClearColor;
+
 class DearImGui
 {
 public:
@@ -26,5 +28,39 @@ public:
         ImGui_ImplDX12_NewFrame();
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
+    }
+
+    void DemoWindows(ClearColor& color, bool& show)
+    {
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show)
+            ImGui::ShowDemoWindow(&show);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show);      // Edit bools storing our window open/close state
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+    }
+
+    void EndFrame()
+    {
+        ImGui::Render();
     }
 };
