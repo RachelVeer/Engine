1b57ba3f2bf7917f75f438e48e1a5f673e691bd0
Rachel Veer <rachelyoubish@gmail.com>
2021-06-11 16:18:04 -0500

    SetGraphicsRootDescriptorTable -> SetGraphicsRootConstantBufferView.

--- Engine/src/Platform/Direct3D/D3D12Context.cpp
+++ Engine/src/Platform/Direct3D/D3D12Context.cpp
@@ -308,7 +308,8 @@ void LoadAssets()
         CD3DX12_ROOT_PARAMETER1 rootParameters[2];
 
         ranges[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_CBV, 1, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DATA_STATIC);
-        rootParameters[0].InitAsDescriptorTable(1, &ranges[0], D3D12_SHADER_VISIBILITY_VERTEX);
+        //rootParameters[0].InitAsDescriptorTable(1, &ranges[0], D3D12_SHADER_VISIBILITY_VERTEX);
+        rootParameters[0].InitAsConstantBufferView(0, 0);
 
         ranges[1].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DATA_STATIC);
         rootParameters[1].InitAsDescriptorTable(1, &ranges[1], D3D12_SHADER_VISIBILITY_PIXEL);
@@ -751,7 +752,9 @@ void PopulateCommandList()
     ID3D12DescriptorHeap* ppHeaps[] = { g_cbvHeap.Get() };
     g_CommandList->SetDescriptorHeaps(_countof(ppHeaps), ppHeaps);
 
-    g_CommandList->SetGraphicsRootDescriptorTable(0, g_cbvHeap->GetGPUDescriptorHandleForHeapStart());
+    //g_CommandList->SetGraphicsRootDescriptorTable(0, g_cbvHeap->GetGPUDescriptorHandleForHeapStart());
+    g_CommandList->SetGraphicsRootConstantBufferView(0, g_ConstantBuffer->GetGPUVirtualAddress());
+
     g_CommandList->SetDescriptorHeaps(1, g_srvHeap.GetAddressOf());
     g_CommandList->SetGraphicsRootDescriptorTable(1, g_srvHeap->GetGPUDescriptorHandleForHeapStart());
     //g_CommandList->SetGraphicsRootDescriptorTable(1, g_cbvHeap->GetGPUDescriptorHandleForHeapStart());
