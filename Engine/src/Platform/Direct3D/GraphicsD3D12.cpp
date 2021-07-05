5d9eeff2129669ffbdd42e2bf5322eede8db5a8a
Rachel Veer <rachelyoubish@gmail.com>
2021-07-05 14:55:40 -0500

    Normal texture mapping restored.

--- Engine/src/Platform/Direct3D/GraphicsD3D12.cpp
+++ Engine/src/Platform/Direct3D/GraphicsD3D12.cpp
@@ -317,9 +317,9 @@ void LoadAssets()
 
         D3D12_SAMPLER_DESC sampler = {};
         sampler.Filter = D3D12_FILTER_MIN_MAG_LINEAR_MIP_POINT;
-        sampler.AddressU = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
-        sampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
-        sampler.AddressW = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
+        sampler.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+        sampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+        sampler.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
         sampler.MipLODBias = 0;
         sampler.MaxAnisotropy = 0;
         sampler.ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
@@ -416,9 +416,9 @@ void LoadAssets()
         {
             // Clockwise.
             { { -0.45f,  0.45f, 0.0f}, { 1.0f, 0.0f, 0.0f, 1.0f }, { 0.0f, 0.0f } }, // top left
-            { {  0.45f, -0.45f, 0.0f}, { 0.0f, 1.0f, 0.0f, 1.0f }, { 2.0f, 2.0f } }, // bottom right
-            { { -0.45f, -0.45f, 0.0f}, { 0.0f, 0.0f, 1.0f, 1.0f }, { 0.0f, 2.0f } }, // bottom left
-            { {  0.45f,  0.45f, 0.0f}, { 1.0f, 1.0f, 0.0f, 1.0f }, { 2.0f, 0.0f } }, // top right
+            { {  0.45f, -0.45f, 0.0f}, { 0.0f, 1.0f, 0.0f, 1.0f }, { 1.0f, 1.0f } }, // bottom right
+            { { -0.45f, -0.45f, 0.0f}, { 0.0f, 0.0f, 1.0f, 1.0f }, { 0.0f, 1.0f } }, // bottom left
+            { {  0.45f,  0.45f, 0.0f}, { 1.0f, 1.0f, 0.0f, 1.0f }, { 1.0f, 0.0f } }, // top right
         };
 
         const uint32_t vertexBufferSize = sizeof(triangleVertices);
